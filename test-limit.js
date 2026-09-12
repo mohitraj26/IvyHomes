993fa9e589d50d94354eb656a0f6b63468a97be3
mohitraj26 <mohitraj26102@gmail.com>
2026-09-12 13:26:41 +0530

    test: validate API and frontend flows

---
+++ test-limit.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+fetch('https://solve.ivy.homes/auth/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json', 'x-api-key': 'IVY26-A196B34278FF' },
+  body: JSON.stringify({ email: 'demo1@ivy.homes', password: '14d181752b' })
+}).then(res => res.json()).then(data => {
+  fetch('https://solve.ivy.homes/v1/listings?limit=6000', {
+    headers: { 'Authorization': 'Bearer ' + data.access_token, 'x-api-key': 'IVY26-A196B34278FF' }
+  }).then(r => r.json()).then(d => console.log(d.results?.length));
+});
